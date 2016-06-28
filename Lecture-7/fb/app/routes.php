1ced1027558ee62d1a3df7f736d088df862fc526
amantrip <am4227@columbia.edu>
2016-06-28 14:17:25 -0400

    Moved Lecture 7 to 8

--- Lecture-7/fb/app/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::get('/login', 'AuthenticationController@showLoginView');
-
-
-Route::post('/login', 'AuthenticationController@loginUser');
-
-Route::get('/users', 'AuthenticationController@showUsers');
